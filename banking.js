5bf0c1413432ec45cb6e28520ad939048bd1a056
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-11 20:56:39 +0600

    Updating balance function added for simplifying updating balance from depositing and withdrawal

--- banking.js
+++ banking.js
@@ -3,8 +3,7 @@ depositBtn.addEventListener('click', function() {
     const depositAmount = getInputValue('deposit-amount');
     adding('deposit-dollars', depositAmount);
     //Updating Balance
-    const balanceAmount = document.getElementById('balance-dollars');
-    balanceAmount.innerText = parseFloat(balanceAmount.innerText) + parseFloat(depositAmount);
+    updateBalance(depositAmount, true);
 })
 
 
@@ -14,12 +13,10 @@ withdrawBtn.addEventListener('click', function() {
     const withdrawAmount = getInputValue('withdraw-amount');
     adding('withdraw-dollars', withdrawAmount);
     //Updating Balance
-    const balanceAmount = document.getElementById('balance-dollars');
-    balanceAmount.innerText = parseFloat(balanceAmount.innerText) - parseFloat(withdrawAmount);
+    updateBalance(withdrawAmount, false);
 })
 
-
-//Functional
+//Function for getting input value
 function getInputValue(input) {
     const inputField = document.getElementById(input);
     const inputAmount = inputField.value;
@@ -27,10 +24,16 @@ function getInputValue(input) {
     return inputAmount;
 
 }
-
 //Adding function
 function adding(input1, input2) {
     const addTotal = document.getElementById(input1);
     addTotal.innerText = parseFloat(addTotal.innerText) + parseFloat(input2);
 
+}
+
+//Updating Balance
+function updateBalance(input, isAdd) {
+    const balanceAmount = document.getElementById('balance-dollars');
+    if (isAdd == true) balanceAmount.innerText = parseFloat(balanceAmount.innerText) + parseFloat(input);
+    else balanceAmount.innerText = parseFloat(balanceAmount.innerText) - parseFloat(input);
 }
