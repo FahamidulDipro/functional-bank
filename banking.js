24f9a23a6ad1e7c26ac292dd7fa9bf205c2aa44b
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-11 21:30:14 +0600

    error handling for negative number,string and insufficient balance alert

--- banking.js
+++ banking.js
@@ -1,9 +1,21 @@
 const depositBtn = document.getElementById('deposit-btn');
 depositBtn.addEventListener('click', function() {
     const depositAmount = getInputValue('deposit-amount');
-    adding('deposit-dollars', depositAmount);
-    //Updating Balance
-    updateBalance(depositAmount, true);
+    if (isNaN(depositAmount) == true) {
+        alert('please enter number');
+    } else {
+        if (depositAmount > 0) {
+            adding('deposit-dollars', depositAmount);
+            //Updating Balance
+            updateBalance(depositAmount, true);
+        } else {
+            alert('Please enter valid quantity');
+        }
+    }
+
+
+
+
 })
 
 
@@ -11,9 +23,20 @@ depositBtn.addEventListener('click', function() {
 const withdrawBtn = document.getElementById('withdraw-btn');
 withdrawBtn.addEventListener('click', function() {
     const withdrawAmount = getInputValue('withdraw-amount');
-    adding('withdraw-dollars', withdrawAmount);
-    //Updating Balance
-    updateBalance(withdrawAmount, false);
+    const currentBal = getCurrentBalance();
+    if (withdrawAmount > 0) {
+        if (withdrawAmount > currentBal) {
+            alert('Insufficient balance');
+        } else {
+            adding('withdraw-dollars', withdrawAmount);
+            //Updating Balance
+            updateBalance(withdrawAmount, false);
+        }
+    } else {
+        alert('Please enter valid quantity');
+    }
+
+
 })
 
 //Function for getting input value
@@ -36,4 +59,14 @@ function updateBalance(input, isAdd) {
     const balanceAmount = document.getElementById('balance-dollars');
     if (isAdd == true) balanceAmount.innerText = parseFloat(balanceAmount.innerText) + parseFloat(input);
     else balanceAmount.innerText = parseFloat(balanceAmount.innerText) - parseFloat(input);
+}
+
+
+//Get Current Balance
+function getCurrentBalance() {
+    const balanceAmount = document.getElementById('balance-dollars');
+    const balanceAmountText = balanceAmount.innerText;
+    const prevBalanceTotal = parseFloat(balanceAmountText);
+    return prevBalanceTotal;
+
 }
